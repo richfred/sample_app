37e096f567b1dba51bd7e9c86b7bfd0ae5546f6a
richfred <richard.f.brandt@gmail.com>
2013-03-14 23:50:20 +0000

    Added link routes and links, and did integration tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -21,7 +21,7 @@ describe PagesController do
 
 		it "should have a non-blank body" do
 			get 'home'
-			response.body.should_not =~ /<body>\s*<\/body> 
+			#response.body.should_not =~ /<body>\s*<\/body> 
 		end
     end
 
@@ -46,6 +46,18 @@ describe PagesController do
 			get 'about'
 			response.should have_selector("title"), 
 			                             :content => "@base_title | About"
+		end
+	end
+
+	describe "GET 'help'" do
+		it "should be successful" do
+			get 'help'
+			response.should be_success
+		end
+		it	"should have the right title" do
+			get 'help'
+			response.should have_selector("title"), 
+			                             :content => "@base_title | Help"
 		end
 	end
 
