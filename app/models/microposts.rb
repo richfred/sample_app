7bb3135aabed670b684207fb0145e5b59d2dd3d0
richfred <richard.f.brandt@gmail.com>
2013-04-28 02:21:16 +0000

    Finished microposts

---
+++ app/models/microposts.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class Microposts < ActiveRecord::Base
+  attr_accessible :content
+
+  belongs_to :user
+end
